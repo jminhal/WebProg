8811c76d5d7015b570d2f206a205c1193770b235
JorgeMinhalma <jorgeminhalma0@gmail.com>
2020-12-21 22:10:41 +0000

    Tutorial Node/Express (DataBase)

--- Models/studentsModel.js
+++ Models/studentsModel.js
@@ -1,50 +1,40 @@
-var students = [
-    { name: "John Smith", number: 201, id: 12, units:[
-        { unit: "Mathematics", grade: 8.3 },
-        { unit: "Literature", grade: 11.2 },
-        { unit: "Laws", grade: 18.5 },
-        { unit: "Informatics", grade: 14.3 },
-        { unit: "Cooking", grade: 7.4 }
-    ] },
+const courseId = 2;
+var pool = require("./connection");
+module.exports.getAllStudents = async function() { 
+    try {
+        const sql = "SELECT alu_id AS id, alu_nome AS name, 0 AS number FROM alunos WHERE alu_cur_id = "+courseId;
+        const students = await pool.query(sql);
+        return students;
+    } catch (err) {
+        return err;
+    } 
+}
+module.exports.getStudent = async function(pos) {
 
-    { name: "Mary Jane", number: 512, id: 31, units:[
-        { unit: "Mathematics", grade: 8.3 },
-        { unit: "Literature", grade: 11.2 },
-        { unit: "Laws", grade: 18.5 }
-    ] },
-
-    { name: "Jane Dow", number: 45, id: 3, units:[
-        { unit: "Mathematics", grade: 8.3 },
-        { unit: "Literature", grade: 11.2 }
-    ] }
-];
-
-module.exports.getAllStudents =function() {
-     return students; }
-
-module.exports.getStudent =function(pos) {
-     return students[pos]; };
+    try {
+        let sql = "SELECT alu_id AS id, alu_nome AS name, 0 AS number FROM alunos WHERE alu_id = ?";
+        let student = await pool.query(sql,[pos]);
+        let infoStudent = student[0];
+        sql = "SELECT dis_id AS id, dis_nome AS name, dis_creditos AS ects, pla_semestre AS semester, ins_nota AS grade, ins_id FROM disciplinas, planoestudos, inscricoes WHERE dis_id = pla_dis_id AND ins_pla_dis_id = pla_dis_id AND ins_alu_id = ? AND pla_cur_id=" +courseId;
+        infoStudent.grades = await pool.query(sql,[pos]);
+        return infoStudent;
+    }
+    catch (err) {
+        console.log(err);
+        return err;
+    }
+};
 
 module.exports.saveGrade = function(pos, unit) {
-    let student = students[pos];
-    let units = student.units;
-
-    if (containsUnit(unit, units, student)) {
-        //Se existir
-        return {msg:"Changed grade of unit " + unit.unit};
-    } else {
-        units.push({ unit: unit.unit, grade: unit.grade });
-        return {msg:"Added grade for unit " + unit.unit};
+    try { 
+        let sql = "UPDATE inscricoes SET ins_dt_avaliacao = ? , ins_nota = ? WHERE ins_id = ?";
+        let result = await pool.query(sql,[ new Date(), unit.grade,unit.id ]);
+        if ( result.changedRows > 0 ) return { msg: "Changed grade of unit " + unit.unit };
+        else return { msg: "ERROR: Iscription does not correspond to the student and/or unit!" };
     }
-
-}
-
-function containsUnit(unit, units, student) {
-    for (let i = 0; i < units.length; i++) {
-        if (unit.unit === units[i].unit) {
-            student.units[i].grade = unit.grade;
-            return  true;
-        }
+    catch (err) {
+        console.log(err);
+        return err;
     }
-    return false;
+    
 }
